b79d8b83f177a6b834303289f125c2c35cd919c7
tamirlanm <tamirlanmirgali@gmail.com>
2026-05-03 19:21:17 +0500

    inital commit

---
+++ OSIkz.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OSIkz.Web.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ OSIkz.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OSIkz.Web.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
